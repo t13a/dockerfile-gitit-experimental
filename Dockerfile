97002a11478f4a152e753eb856cfaa0107c5cc79
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2020-06-28 10:42:42 +0900

    Switch additional script in entrypoint

--- Dockerfile
+++ Dockerfile
@@ -32,4 +32,3 @@ RUN apt-get update \
 
 COPY rootfs /
 
-ENTRYPOINT [ "/entrypoint-experimental.sh" ]
--- rootfs/entrypoint.d/99-gitit-bootstrap-theme.sh
+++ rootfs/entrypoint.d/99-gitit-bootstrap-theme.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-/entrypoint.sh true
-
 GITIT_BOOTSTRAP_THEME_DIR=/opt/gitit-bootstrap-theme
 GITIT_STATIC_DIR="${GITIT_WORK_DIR}/static"
 GITIT_TEMPLATES_DIR="${GITIT_WORK_DIR}/templates"
@@ -20,11 +18,3 @@ then
     chown -R "${GITIT_USER}:${GITIT_USER}" "${GITIT_TEMPLATES_DIR}"
 fi
 
-cd "${GITIT_WORK_DIR}"
-
-if [ ${#@} -eq 0 ]
-then
-    exec gosu "${GITIT_USER}" gitit -f "${GITIT_CONF}"
-else
-    exec "${@}"
-fi
